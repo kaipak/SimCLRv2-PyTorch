1c8126f82d84a56fffdc7761920750943f9c21a1
Kai Pak <kaipak@jpl.nasa.gov>
2021-08-30 18:10:04 -0700

    Config and util stuff needed for argparse.

--- simclr/simclr.py
+++ simclr/simclr.py
@@ -1,13 +1,10 @@
 import torch.nn as nn
-import torchvision
-
-from simclr.modules.resnet_hacks import modify_resnet_model
 from simclr.modules.identity import Identity
-
 
 class SimCLR(nn.Module):
     """
-    We opt for simplicity and adopt the commonly used ResNet (He et al., 2016) to obtain hi = f(x ̃i) = ResNet(x ̃i) where hi ∈ Rd is the output after the average pooling layer.
+    We opt for simplicity and adopt the commonly used ResNet (He et al., 2016) to obtain hi = f(x ̃i) = ResNet(x ̃i)
+    where hi ∈ Rd is the output after the average pooling layer.
     """
 
     def __init__(self, encoder, projection_dim, n_features):
@@ -19,7 +16,8 @@ class SimCLR(nn.Module):
         # Replace the fc layer with an Identity function
         self.encoder.fc = Identity()
 
-        # We use a MLP with one hidden layer to obtain z_i = g(h_i) = W(2)σ(W(1)h_i) where σ is a ReLU non-linearity.
+        # We use a MLP with one hidden layer to obtain z_i = g(h_i) = W(2)σ(W(1)h_i)
+        # where σ is a ReLU non-linearity.
         self.projector = nn.Sequential(
             nn.Linear(self.n_features, self.n_features, bias=False),
             nn.ReLU(),
