cb900a5f5f92a1f9d8bb0f7f650117abfbc600d7
Kai Pak <kai@kaipak.org>
2021-09-28 18:42:19 -0700

    Create fine tune SimCLR class.

--- simclr/simclr.py
+++ simclr/simclr.py
@@ -35,21 +35,19 @@ class SimCLR(nn.Module):
     def forward(self, x_i, x_j):
         h_i = self.encoder(x_i)
         h_j = self.encoder(x_j)
-
         z_i = self.projector(h_i)
         z_j = self.projector(h_j)
         return h_i, h_j, z_i, z_j
 
 
 class SimCLRv2(nn.Module):
+    """SimCLRv2 Implementation
+        Using ResNet architecture from Pytorch converter which includes projection head.
+
+    """
     def __init__(self, resnet_depth: int = 50, resnet_width_multiplier: int = 2,
                  sk_ratio: float = 0.0625, pretrained_weights: str = None):
-        """SimCLRv2 Implementation
-            Using ResNet architecture from Pytorch converter which includes projection head.
-
-        """
         super(SimCLRv2, self).__init__()
-
         self.encoder, self.projector = get_resnet_v2(depth=resnet_depth,
                                                      width_multiplier=resnet_width_multiplier,
                                                      sk_ratio=sk_ratio)
@@ -61,9 +59,27 @@ class SimCLRv2(nn.Module):
         """"""
         h_i = self.encoder(x_i)
         h_j = self.encoder(x_j)
-        foo = torch.nn.Sequential(*(list(self.projector.children())[0][:3]))(h_i)
+        foo = torch.nn.Sequential(*(list(self.projector.children())[0][:]))(h_i)
         z_i = self.projector(h_i)
-        print(torch.all(foo==z_i))
         z_j = self.projector(h_j)
 
         return h_i, h_j, z_i, z_j
+
+class SimCLRv2_ft(nn.Module):
+    """Take a pretrained SimCLRv2 Model and Finetune with linear layer"""
+    def __init__(self, simclrv2_model):
+        self.encoder = simclrv2_model.encoder
+        # From v2 paper, we just need the first layer from projector
+        self.projector = torch.nn.Sequential(*(list(simclrv2_model.projector.children())[0][:2]))
+        # Hack
+        linear_in_features = self.projector[0].out_features
+        self.linear = nn.Linear(linear_in_features)
+
+    def forward(self, x):
+        h = self.encoder(x)
+        h_prime = self.projector(h)
+        y_hat = self.linear(h_prime)
+
+        return y_hat
+
+
